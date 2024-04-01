af859b50be74a3284271883c4fc23e6ceb9048ff
Giovani Damian <60575219+GiovaniDamian@users.noreply.github.com>
2024-03-31 22:11:34 -0300

    Update script.js
    add animation

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
     const toggleButton = document.getElementById('toogleBtn');
+    const jumpBtn = document.getElementById('up');
+    const crouchBtn = document.getElementById('down');
 
     toggleButton.addEventListener('click', function() {
     const elementsShow = document.querySelectorAll('[show]');
@@ -14,4 +16,16 @@ document.addEventListener('DOMContentLoaded', function() {
             }
         );
     });
+    
+   crouchBtn.addEventListener('click', function() {
+        document.getElementById("rabbit").style.cssText = 'top:130px; transition: all 0.2s  ease-out; transform : rotate(-95deg);  animation: play1 3s steps(3) infinite;';
+setTimeout(function(){document.getElementById("rabbit").style.cssText = 'transition: all 1s  ease-out;'; document.getElementById("rabbit").style.top = "60px";},500);
+    })
+
+    jumpBtn.addEventListener('click',function () {
+document.getElementById("rabbit").style.cssText = 'transition: all 0.3s  ease-out;'; 
+document.getElementById("rabbit").style.top = "-30px";
+setTimeout(function(){document.getElementById("rabbit").style.top = "60px";},200);
+} )
+
 });
