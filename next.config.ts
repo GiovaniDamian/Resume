454b02f8de985b753a9b59e34ca960e302f3b41b
Giovani Damian <gedamian@gmail.com>
2024-04-26 21:14:15 -0300

    change next.config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,12 @@
+import * as path from 'path';
+
 const basePath = process.env.NODE_ENV === 'production' ? '/Resume' : '';
 
 module.exports = {
   basePath,
   assetPrefix: basePath,
   pageExtensions: ['tsx', 'jsx', 'js', 'ts'],
-  webpack: (config, { isServer }) => {
+  webpack: (config:any, { isServer } : any) => {
     if (!isServer) {
       config.resolve.alias['@'] = path.resolve(__dirname, 'src');
     }
