a4c0f194585357863a752664966e3611f87320ad
Giovani Damian <gedamian@gmail.com>
2024-04-28 12:19:15 -0300

    try fix mobile production problem

--- components/template/ButtonWelcome.tsx
+++ components/template/ButtonWelcome.tsx
@@ -9,7 +9,7 @@ interface ButtonWelcomeProps {
 export default function ButtonWelcome({ onClick, label, isActive }: ButtonWelcomeProps) {
 	return (
 		<div className={`border-button bg-gradient-to-b from-mainGrayBlue to-mainBlue ${isActive ? "opacity-25 lg:opacity-85" : "opacity-90"}`}>
-			<button className={`hex-button relative h-24 w-32 text-base px-6 py-2 cursor-pointer outline-none transition-all duration-300 ease-in clip-path-polygon hover:bg-gradient-to-b from-mainGrayBlue to-mainBlue hover:transform-scale-105 ${isActive ? 'bg-mainBlue' : 'bg-white'}`} onClick={onClick}>
+			<button className={`hex-button relative h-24 w-32 text-base px-6 py-2 cursor-pointer outline-none transition-all duration-300 ease-in clip-path-polygon hover:bg-gradient-to-b from-mainGrayBlue to-mainBlue hover:transform-scale-105 300:text-black ${isActive ? 'bg-mainBlue' : 'bg-white'}`} onClick={onClick}>
 				<span>{label}</span>
 			</button>
 		</div>
